915a77cbfeb37e8d150d858f32fde912f7f60ff3
Jam Risser <jamrizzi@gmail.com>
2016-08-15 20:10:10 -0500

    first functional version

--- deskterm.lua
+++ deskterm.lua
@@ -1,6 +1,10 @@
 if (get_application_name()=="Terminal") then
 	if (get_window_role()=="DesktopTerminal") then
 		debug_print("DesktopTerminal is open");
+		undecorate_window();
+		pin_window();
+		set_window_fullscreen(false);
+		set_window_position2(0,0);
+		maximize();
 	end
 end
-
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt-get install -y devilspie2
 
 tee -a /etc/skel/.profile<<EOF
-gnome-terminal --window-with-profile=DesktopTerminal --role=DesktopTerminal
+gnome-terminal --window-with-profile=DesktopTerminal --role=DesktopTerminal --hide-menubar
 EOF
 
 mv deskterm.lua /etc/skel/.config/devilspie2/
