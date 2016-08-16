3f42abd7f86b473cc654dc606eb606df25c7ad5b
Jam Risser <jamrizzi@users.noreply.github.com>
2016-08-15 20:17:01 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 apt-get install -y devilspie2
 
 tee -a /etc/skel/.profile<<EOF
+devilspie2
+sleep 4s
 gnome-terminal --window-with-profile=DesktopTerminal --role=DesktopTerminal --hide-menubar
 EOF
 
+mkdir /etc/skel/.config/
+mkdir /etc/skel/.config/devilspie2/
 mv deskterm.lua /etc/skel/.config/devilspie2/
-
