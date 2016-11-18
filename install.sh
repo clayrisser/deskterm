4b154cd4ba8fef42a128c3588868c8bc068c9d7e
Jam Risser <jamrizzi@users.noreply.github.com>
2016-11-17 20:15:56 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
 apt-get install -y devilspie2
 
 tee -a ~/.profile<<EOF
-devilspie2
+devilspie2 &
 sleep 4s
 gnome-terminal --window-with-profile=Deskterm --role=Deskterm --hide-menubar
 EOF
 
 mkdir -p ~/.config/devilspie2/
-mv deskterm.lua ~/.config/devilspie2/
-
-cd ../ && rm -rf ./deskterm/
+wget -P ~/.config/devilspie2/ https://raw.githubusercontent.com/jamrizzi/deskterm/master/deskterm.lua
