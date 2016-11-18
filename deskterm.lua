4f7c6997cf17f35b7df9524b7126006a4ca1fba5
Jam Risser <jamrizzi@users.noreply.github.com>
2016-11-17 20:14:54 -0600

    Update deskterm.lua

--- deskterm.lua
+++ deskterm.lua
@@ -1,6 +1,6 @@
 if (get_application_name()=="Terminal") then
 	if (get_window_role()=="Deskterm") then
-		set_window_position2(0, 0);
+		set_window_position(0, 0);
 		maximize();
 		set_window_type("WINDOW_TYPE_DESKTOP");
 		pin_window();
